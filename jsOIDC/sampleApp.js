9a1daf90bdb584cd3e8ceebb3df0ef9b60475c19
tamilanbann <72143916+tamilanbann@users.noreply.github.com>
2023-12-11 11:47:14 +0530

    Update sampleApp.js

--- jsOIDC/sampleApp.js
+++ jsOIDC/sampleApp.js
@@ -1,43 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Redirect, useLocation } from 'react-router-dom';
-import { userManager } from './oidcService';
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import userManager from './userManager';
 import Login from './Login'; // Your login component
 import Dashboard from './Dashboard'; // Your protected component
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const location = useLocation();
+  const isAuthenticated = !!userManager.getUser() && !userManager.getUser().expired;
 
-  useEffect(() => {
-    const checkAuthentication = async () => {
-      const user = await userManager.getUser();
-      setIsAuthenticated(!!user);
-      setIsLoading(false);
-    };
+  const handleLogin = () => {
+    userManager.signinRedirect();
+  };
 
-    checkAuthentication();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const handleLogout = () => {
+    userManager.signoutRedirect();
+  };
 
   return (
-    <Route
-      render={() =>
-        isAuthenticated ? (
-          <Route path="/dashboard" component={Dashboard} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: '/login',
-              state: { from: location.pathname },
-            }}
-          />
-        )
-      }
-    />
+    <Router>
+      <Route
+        path="/login"
+        render={() => (isAuthenticated ? <Redirect to="/dashboard" /> : <Login />)}
+      />
+      <Route
+        path="/dashboard"
+        render={() => (isAuthenticated ? <Dashboard /> : <Redirect to="/login" />)}
+      />
+    </Router>
   );
 };
 
